5b7db3e7bba551fd2360b7d880dcc60426fa5486
Frameck <fra9879@gmail.com>
2021-12-17 16:15:41 +0100

    Completed exercise

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/home", function () {
+    $data = [
+        'title' => 'Lista spesa',
+        'list' => [
+            'Pane',
+            'Pasta',
+            'Mozzarella',
+            'Vino',
+            'Pollo'
+        ]
+    ];
+
+    return view("home", $data);
+})->name("homePage");
